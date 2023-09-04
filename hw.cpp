7a1711a27eafa5a46e13842a9d5571c5b79eff46
kingston-smj <kingstonsmj@gmail.com>
2023-09-04 20:36:31 +0100

    new file for torrent vid

---
+++ hw.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    
+    std::string name = "kingston";
+    std::cout << "hope?" << '\n';
+    std::cout << name;
+    return 0;
+}
---
+++ start.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main()
+{
+    std::string name = "Kingston";
+    
+    std::cout << "Hello, " << name;
+
+
+
+    return 0;
+}
